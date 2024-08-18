1cdcb008f4c43bdc84edb7193aa3c26c82c4428c
Charles-Ian <Charlesianbarral1234@gmail.com>
2024-08-18 22:50:54 +0700

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfo
+{
+	public static void main(String[] args) {
+	    
+	    String movieQuote = "'I believe whatever doesn't kill you simply makes you a stranger.'";
+	    String movieFrom  = "Produced by 20th Century Fox and Paramount Pictures";
+	    String movieCharacter = "It’s said by the character Joker, played by Jack Oswald White";
+	    String movieYear = "Joker (2019)";
+	    
+	    System.out.println("\n" + movieQuote + "\n" + movieFrom +"\n" + movieCharacter + "\n" + movieYear);
+	}
+}
