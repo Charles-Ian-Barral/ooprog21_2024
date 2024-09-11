83679997a7b8074c25d3b530bbaa54c24c1fd112
Charles Ian Barral <Charlesianbarral1234@gmail.com>
2024-09-11 08:09:51 +0800

    Prelim.java

--- PRELIM/Prelim.java
+++ PRELIM/Prelim.java
@@ -2,10 +2,10 @@
  
   public static void main(String[] args) {  
 
-    int n = 13, firstNumber = 0, secondNumber = 1;
-    System.out.println("Fibonacci (0) " + n + " Number:");
+    int firstNumber = 0, secondNumber = 1;
+    System.out.println("Fibonacci (0) 14" + " Number:");
 
-    for (int i = 0; i <= n; ++i) {
+    for (int i = 0; i <= 14; ++i) {
       System.out.print(firstNumber + ", ");
 
       int nextNumber = firstNumber + secondNumber;
