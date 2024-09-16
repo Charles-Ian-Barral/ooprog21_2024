0c9fe1ec58238e0e980b98a0facd2a689e210c54
Charles Ian Barral <Charlesianbarral1234@gmail.com>
2024-09-16 22:33:24 +0700

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,13 @@
+public class FirstAddressExercise {
+    public static void displayAddress() {
+        System.out.println("Huawei Technologies");
+        System.out.println("Ababa,Ethiopia");
+        System.out.println("P.O.Box 25536 code 1000,Addis");
+        System.out.println("Company: Huawei Technologies Co., LTD（Ethiopia）.");
+        
+        
+    }
+    public static void main(String[] args){
+        displayAddress();
+            }
+}
